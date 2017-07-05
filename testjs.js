9a67b5f166988ba8ac3a2c265c9b2328651ed8a8
akhiljcts <akhiljoy.kandamkulathy@cognizant.com>
2017-07-05 18:32:54 +0530

    yes
    yesd

---
+++ testjs.js
@@ -0,0 +1,8 @@
+<script>
+function myFunction() {
+    document.getElementById("myH2").style.color = "#ff0000";
+    document.getElementById("myP").style.color = "magenta";
+    document.getElementById("myP2").style.color = "blue";
+    document.getElementById("myDiv").style.color = "lightblue";
+}
+</script>
